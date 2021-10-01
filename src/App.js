e1f3b3fd261a7e2ab915dd3dfd85594fd1ae9df5
blackninja81 <johnkaburu81@gmail.com>
2021-10-01 19:55:17 +0300

    complete 6 sections

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import Homepage from "./Pages/Homepage";
+
 function App() {
   return (
     <div className="App">
-     
+     <Homepage/>
     </div>
   );
 }
---
+++ src/Pages/Homepage.js
@@ -0,0 +1,70 @@
+
+function Homepage() {
+    return (
+        <div>
+            <div className='sec1'>
+                <div className='sec1-data'>
+                    <img className='img1' src={'http://www.alfazance.com/assets/images/home/section-1-logo.png'} />
+                    <img className='img2' src={'http://www.alfazance.com/assets/images/home/section-1-title.png'} />
+                    <div className='title1'>
+                        <h4>Alfazance Consulting celebrates 5 Years of Customer Service and Experience</h4>
+                    </div>
+                </div>
+            </div>
+            <div className='sec2'>
+                <div className='right-text'>
+                    <h1>Alfazance now a Bin Hayl Group Company</h1>
+                    <h5>Oman Based Bin Hayl Group; a diversified Business House in Manufacturing,
+                        Real Estate, Agriculture and Down Stream O&G acquires shares in Alfazance
+                        Consulting.
+                    </h5>
+                    <button className='btn-primary'>Learn More</button>
+                </div>
+            </div>
+            <div className='sec3'>
+                <div className='left-text'>
+                    <h1>Alfazance now a Bin Hayl Group Company</h1>
+                    <h5>Oman Based Bin Hayl Group; a diversified Business House in Manufacturing,
+                        Real Estate, Agriculture and Down Stream O&G acquires shares in Alfazance
+                        Consulting.
+                    </h5>
+                    <button className='btn-primary'>Learn More</button>
+                </div>
+            </div>
+            <div className='sec4'>
+                <div className='right-text'>
+                    <h1>Alfazance now a Bin Hayl Group Company</h1>
+                    <h5>Oman Based Bin Hayl Group; a diversified Business House in Manufacturing,
+                        Real Estate, Agriculture and Down Stream O&G acquires shares in Alfazance
+                        Consulting.
+                    </h5>
+                    <button className='btn-primary'>Learn More</button>
+                </div>
+            </div>
+            <div className='sec5'>
+                <div className='left-text'>
+                    <h1>Alfazance now a Bin Hayl Group Company</h1>
+                    <h5>Oman Based Bin Hayl Group; a diversified Business House in Manufacturing,
+                        Real Estate, Agriculture and Down Stream O&G acquires shares in Alfazance
+                        Consulting.
+                    </h5>
+                    <button className='btn-primary'>Learn More</button>
+                </div>
+            </div>
+            <div className='sec6'>
+                <div className='right-text'>
+                    <h1>Alfazance now a Bin Hayl Group Company</h1>
+                    <h5>Oman Based Bin Hayl Group; a diversified Business House in Manufacturing,
+                        Real Estate, Agriculture and Down Stream O&G acquires shares in Alfazance
+                        Consulting.
+                    </h5>
+                    <button className='btn-primary'>Learn More</button>
+                </div>
+            </div>
+            <div className='sec7'>
+
+            </div>
+        </div>
+    )
+}
+export default Homepage;
