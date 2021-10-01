8255df79e1520b0493924b2539d47c3783f3017e
blackninja81 <johnkaburu81@gmail.com>
2021-10-01 20:02:00 +0300

    updated images

--- src/Pages/Homepage.js
+++ src/Pages/Homepage.js
@@ -4,8 +4,8 @@ function Homepage() {
         <div>
             <div className='sec1'>
                 <div className='sec1-data'>
-                    <img className='img1' src={'http://www.alfazance.com/assets/images/home/section-1-logo.png'} />
-                    <img className='img2' src={'http://www.alfazance.com/assets/images/home/section-1-title.png'} />
+                    <img className='img1' alt='home' src={'http://www.alfazance.com/assets/images/home/section-1-logo.png'} />
+                    <img className='img2' alt='home' src={'http://www.alfazance.com/assets/images/home/section-1-title.png'} />
                     <div className='title1'>
                         <h4>Alfazance Consulting celebrates 5 Years of Customer Service and Experience</h4>
                     </div>
@@ -23,40 +23,45 @@ function Homepage() {
             </div>
             <div className='sec3'>
                 <div className='left-text'>
-                    <h1>Alfazance now a Bin Hayl Group Company</h1>
-                    <h5>Oman Based Bin Hayl Group; a diversified Business House in Manufacturing,
-                        Real Estate, Agriculture and Down Stream O&G acquires shares in Alfazance
-                        Consulting.
+                    <h1>Accelerate your journey into the cloud with Alfazance (IMaaS)</h1>
+                    <h5>Implementation as a Service (IMaaS) is an offering for Small Enterprises
+                        opting On-Cloud Licensing Model on Microsoft Business Central, Microsoft Dynamics
+                        D365 CRM or Steeples HCPM Payroll. The implementation is strictly based on
+                        preconfigured templates with scope being accomplished in shorter timeframe window.
+                        To know more about the model check Alfahub.
                     </h5>
                     <button className='btn-primary'>Learn More</button>
                 </div>
             </div>
             <div className='sec4'>
                 <div className='right-text'>
-                    <h1>Alfazance now a Bin Hayl Group Company</h1>
-                    <h5>Oman Based Bin Hayl Group; a diversified Business House in Manufacturing,
-                        Real Estate, Agriculture and Down Stream O&G acquires shares in Alfazance
-                        Consulting.
+                    <h1>Sneak peek into e-Manufacturing</h1>
+                    <h5>Transformational shift towards automation is significant to deliver
+                        velocity and provide customers with solutions at economical costs
+                        and does much more than mere Lean Manufacturing.
+                        We help you to automate your Manufacturing unit and provide sustainable value.
                     </h5>
                     <button className='btn-primary'>Learn More</button>
                 </div>
             </div>
             <div className='sec5'>
                 <div className='left-text'>
-                    <h1>Alfazance now a Bin Hayl Group Company</h1>
-                    <h5>Oman Based Bin Hayl Group; a diversified Business House in Manufacturing,
-                        Real Estate, Agriculture and Down Stream O&G acquires shares in Alfazance
-                        Consulting.
+                    <h1>Build a customer centric retail organization through Alfazance Customer 360</h1>
+                    <h5>Customer experience, loyalty, social media strategies and decision-making data
+                        will continue to rule the future of retail. Our solutions help you to be abreast
+                        with the future and unlock the true TCO.
                     </h5>
                     <button className='btn-primary'>Learn More</button>
                 </div>
             </div>
             <div className='sec6'>
                 <div className='right-text'>
-                    <h1>Alfazance now a Bin Hayl Group Company</h1>
-                    <h5>Oman Based Bin Hayl Group; a diversified Business House in Manufacturing,
-                        Real Estate, Agriculture and Down Stream O&G acquires shares in Alfazance
-                        Consulting.
+                    <h1>Alfazance guides you to convert data into insights</h1>
+                    <h5>Turning data into a strategic asset is business imperative in today’s
+                        world and Information becomes strategic only when it’s used to make
+                        informed decisions. We provide tools that convert strategic information
+                        into actionable insights and our solutions are designed to expose rich
+                        insights that make sense and are logical.
                     </h5>
                     <button className='btn-primary'>Learn More</button>
                 </div>
